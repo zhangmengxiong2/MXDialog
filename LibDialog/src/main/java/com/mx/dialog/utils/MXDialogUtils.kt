a548e36094cf6bcb4256ceaf4a34789bae991ac0
zhangmengxiong <zmx_final@163.com>
2021-10-15 14:49:21 +0800

    Update MXDialogUtils.kt

--- LibDialog/src/main/java/com/mx/dialog/utils/MXDialogUtils.kt
+++ LibDialog/src/main/java/com/mx/dialog/utils/MXDialogUtils.kt
@@ -4,15 +4,24 @@ import android.content.Context
 import kotlin.math.roundToInt
 
 object MXDialogUtils {
+    /**
+     * dp - px单位转换
+     */
     fun dp2px(context: Context, dp: Int): Int {
         return (dp * context.resources.displayMetrics.density + 0.5f).roundToInt()
     }
 
+    /**
+     * 获取顶部状态栏高度：px
+     */
     fun getStatusBarHeight(context: Context): Int {
         val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
         return if (resourceId > 0) context.resources.getDimensionPixelSize(resourceId) else 0
     }
 
+    /**
+     * 获取底部导航栏高度：px
+     */
     fun getNavigationBarHeight(context: Context): Int {
         val resourceId =
             context.resources.getIdentifier("navigation_bar_height", "dimen", "android")
