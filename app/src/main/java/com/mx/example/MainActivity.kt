561bb001477b4d921c8b1b25c9321de7ad14d1e8
zhangmengxiong <zmx_final@163.com>
2022-01-26 15:37:23 +0800

    Update MainActivity.kt

--- app/src/main/java/com/mx/example/MainActivity.kt
+++ app/src/main/java/com/mx/example/MainActivity.kt
@@ -66,8 +66,8 @@ class MainActivity : AppCompatActivity() {
 //            setTitle("请选择")
 //            setContentMaxHeightRatio(1.5f)
 //            setContentCornerRadius(0f)
-//            setContentPosition(MXDialogPosition.CENTER)
-            setContentMargin(horizontal = 50f, vertical = 10f)
+            setContentPosition(MXDialogPosition.CENTER)
+            setContentMargin(horizontal = 20f, vertical = 10f)
 //            setCancelable(false)
             val list = ('A'..'Z').toMutableList().map { it.toString() }
             setMultipleItems(
